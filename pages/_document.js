1ab1039219f31a32945373a15632841f7ef27335
jejakofazhr <somakaton25@gmail.com>
2025-06-07 16:07:38 +0700

    added new file

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link
             rel="preload"
-            href="/fonts/Inter-Regular.woff2"
+            href="/fonts/valo_font.ttf"
             as="font"
             type="font/woff2"
             crossOrigin="anonymous"
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import Link from 'next/link';
+import profilePic from '../public/profile.png';
 
 const HeavyComponent = dynamic(() => import('../components/HeavyComponent'), {
   loading: () => <p>Loading...</p>, 
@@ -12,12 +13,7 @@ export default function Home() {
   return (
     <>
     <h1>Profil</h1>
-      <Image
-        src="/profile.png"
-        alt="Profil"
-        width={300}
-        height={300}
-      />
+    <Image src={profilePic} alt="Profil" width={300} height={300} priority />
       <h1>Beranda</h1>
       <Link href="/posts">Lihat Post</Link>
       <HeavyComponent />
